f3b13bb5ae95506661455011fff5137777686fa3
Daniel De Oliveira <daniel.deoliveira@correo.unimet.edu.ve>
2025-04-03 21:23:35 -0400

    📌 New version of portfolio

--- dani19866.github.io/src/App.jsx
+++ dani19866.github.io/src/App.jsx
@@ -1,18 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Index from "./routes/Index.jsx"
-import Projects from './routes/Projects.jsx';
-import Skills from './routes/Skills.jsx';
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/projects" pa element={<Projects />} />
-        <Route path="/skills" element={<Skills />} />
-      </Routes>
-    </Router>
+    <>
+    </>
   )
 }
 
