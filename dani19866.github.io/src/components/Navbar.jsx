b8dd64d638450bf13984bc7b6d7b018030b44481
Daniel De Oliveira <daniel.deoliveira@correo.unimet.edu.ve>
2025-04-07 00:13:53 -0400

    💾 Navbar component 80%

--- dani19866.github.io/src/components/Navbar.jsx
+++ dani19866.github.io/src/components/Navbar.jsx
@@ -2,10 +2,27 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className="">
-        <p className='text-3xl font-bold'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime soluta perspiciatis ratione dolorum dicta harum blanditiis doloribus! Repellendus, perferendis accusantium et nulla ab doloremque qui, ad maxime, eius labore cupiditate.
-        </p>
+    <div className="font-[Manrope]">
+      <div className='w-full py-2 px-5 shadow'>
+        {/* Content */}
+        <div className="flex justify-between items-center w-full">
+
+          {/* Brand */}
+          <div className="flex flex-col items-center h-full">
+            <h2 className="font-bold uppercase text-base">Daniel De Oliveira</h2>
+            <h3 className="uppercase text-xs">Ingeniero en Sistemas</h3>
+          </div>
+
+          {/* Buttons */}
+          <div className="flex flex-row h-full gap-x-6 items-center justify-center">
+            <a className="uppercase hover:cursor-pointer hover:text-gray-500">Inicio</a>
+            <a className="uppercase hover:cursor-pointer hover:text-gray-500">Estudios</a>
+            <a className="uppercase hover:cursor-pointer hover:text-gray-500">Experiencia</a>
+            <a className="uppercase hover:cursor-pointer hover:text-gray-500">Proyectos</a>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
