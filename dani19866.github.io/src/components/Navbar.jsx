f1035f2dc3d96c706bbe07bbec00e8809fe85bcc
Daniel De Oliveira <daniel.deoliveira@correo.unimet.edu.ve>
2025-04-06 23:50:25 -0400

    💾 Assets and components structure

---
+++ dani19866.github.io/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="">
+        <p className='text-3xl font-bold'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime soluta perspiciatis ratione dolorum dicta harum blanditiis doloribus! Repellendus, perferendis accusantium et nulla ab doloremque qui, ad maxime, eius labore cupiditate.
+        </p>
+    </div>
+  )
+}
