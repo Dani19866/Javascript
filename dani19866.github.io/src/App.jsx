2d556a3dcb18079ce9ab4b7ecde2015a4a58ab5a
Daniel De Oliveira <daniel.deoliveira@correo.unimet.edu.ve>
2025-04-12 17:46:00 -0400

    💾 Experience info  only to +768px

--- dani19866.github.io/src/App.jsx
+++ dani19866.github.io/src/App.jsx
@@ -1,21 +1,36 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import CardSmall from "./components/CardSmall"
+import CardFifty from "./components/CardFifty"
+import data from "./data/data.json"
 
 // Todo responsive desde md:[760px], lg:[1024px] y xl:[1280px]
 function App() {
   return (
     <div className="font-[Manrope]">
+
+      {/* Tables, laptops & monitors */}
       <div className="md:block hidden">
         <Navbar />
 
-        <div className="py-5 px-10 flex flex-col gap-4 ">
+        <div className="py-5 px-10 flex flex-col gap-4">
           <Hero />
           <Card />
+          <CardSmall>
+          {data.map((card, index) => ( // Cambiado data.cards por data directamente
+              <CardFifty
+                key={card.id}
+                className={index % 2 === 0 ? "" : "rotate-180"}
+                cardData={card}
+              />
+            ))}
+          </CardSmall>
         </div>
 
       </div>
 
+      {/* Mobile exception */}
       <div className="w-full flex justify-center items-center">
         <div className="md:hidden flex flex-col justify-center items-center h-dvh text-lg p-4 text-center gap-2 max-w-[415px]">
           <span className="block font-bold">¡Lo siento!</span>
--- dani19866.github.io/src/components/Card.jsx
+++ dani19866.github.io/src/components/Card.jsx
@@ -9,8 +9,8 @@ export default function Card() {
             <img src={historyBackground} alt="" className='absolute z-10 w-full h-full opacity-65 xl:scale-125' />
 
             {/* Content */}
-            <div className='relative p-4 <-'>
-                <h2 className="text-5xl font-bold">Estudios</h2>
+            <div className='relative p-4'>
+                <h2 className="text-5xl font-bold">Estudios </h2>
 
                 <div className='py-32 flex justify-center items-center'>
                     <img src={timeline} alt="" className='w-[1214px]'/>
