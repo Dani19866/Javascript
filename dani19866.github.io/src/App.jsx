f0ce5a9828363fe4c62d9ffce25d45d573812c83
Daniel De Oliveira <daniel.deoliveira@correo.unimet.edu.ve>
2025-04-14 14:08:50 -0400

    💾 Experience 80%

--- dani19866.github.io/src/App.jsx
+++ dani19866.github.io/src/App.jsx
@@ -3,7 +3,11 @@ import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
 import CardInfo from "./components/CardInfo"
+import CardExperience from "./components/CardExperience"
+
+// JSON
 import dutties from "./data/dutties.json"
+import experiences from "./data/experience.json"
 
 // Todo responsive desde md:[760px], lg:[1024px] y xl:[1280px]
 function App() {
@@ -13,14 +17,27 @@ function App() {
 
       <div className="py-5 px-10 flex flex-col gap-4">
         <Hero />
-        <Card title="¿Qué puedo ofrecerle a tu empresa?" classNameTitle="text-center" className='relative p-4 flex flex-col gap-y-12 justify-center items-center'>
+        <Card title="¿Qué puedo ofrecerle a tu empresa?" classNameTitle="text-center" className='relative p-4 flex flex-col gap-y-12 justify-center items-center pb-10'>
           {
             dutties.map((duty) => (
               <CardInfo
                 key={duty.id}
                 duty={duty}
               />
-            ))}
+            ))
+          }
+        </Card>
+        <Card title="Experiencia" className='relative p-4 flex flex-col gap-y-12 justify-center items-center pb-10'>
+          <div className="flex flex-row gap-x-16">
+            {
+              experiences.map((experience) => (
+                <CardExperience
+                  key={experience.id}
+                  experience={experience}
+                />
+              ))
+            }
+          </div>
         </Card>
       </div>
 
---
+++ dani19866.github.io/src/components/CardExperience.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import unimetLogo from "../assets/images/unimet.png"
+import santosLogo from "../assets/images/santos.png"
+
+const logos = {
+    unimetLogo: unimetLogo,
+    santosLogo: santosLogo
+};
+
+export default function CardExperience({ experience }) {
+    return (
+        <div className='w-1/2 max-w-[660px] boxShadow relative overflow-hidden '>
+            <div className='relative flex flex-col justify-center gap-4 h-full p-4 z-10'>
+                <img src={logos[experience.logo]} alt="" srcset="" className="object-contain h-[95px] w-fit" />
+                <h2 className='text-3xl font-medium'>{experience.title}</h2>
+                
+                <div className='flex flex-col gap-y-3'>
+                    {/* Tiempo */}
+                    <div>
+                        <p>
+                            <span className='font-bold'>Tiempo:</span> {experience.time}
+                        </p>
+                    </div>
+
+                    {/* Marco de trabajo */}
+                    <div>
+                        <p>
+                            <span className='font-bold'>Marco de trabajo:</span> {experience.framework}
+                        </p>
+                    </div>
+
+                    {/* Responsabilidades */}
+                    <div>
+                        <p>
+                            <span className='font-bold'>Responsabilidades:</span> {experience.responsibilities}
+                        </p>
+                    </div>
+
+                    {/* Descripción */}
+                    <div>
+                        <p className="leading-7">
+                            {experience.description}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
